fb43e0826e55d6a90f476bbf85a11fb992ab732f
deepblue_zou <1023509177@qq.com>
2017-09-01 16:27:36 +0800

    增加短点击,长点击接口

--- app/src/main/java/com/prohua/demoloadmore/DefaultAdapter.java
+++ app/src/main/java/com/prohua/demoloadmore/DefaultAdapter.java
@@ -1,6 +1,7 @@
 package com.prohua.demoloadmore;
 
 import android.content.Context;
+import android.view.View;
 
 import java.util.List;
 
@@ -16,8 +17,25 @@ public class DefaultAdapter extends DefaultRVAdapter {
     }
 
     @Override
-    protected void onBindItemViewHolder(DefaultViewHolder holder, int position) {
-        onBindItemView.onBindItemViewHolder(holder,position);
+    protected void onBindItemViewHolder(DefaultViewHolder holder, final int position) {
+        onBindItemView.onBindItemViewHolder(holder, position);
+        if (onBindItemClick != null) {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBindItemClick.onBindItemClick(v, position);
+                }
+            });
+        }
+        if (onBindItemLongClick != null) {
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    onBindItemLongClick.onBindItemLongClick(v, position);
+                    return true;
+                }
+            });
+        }
     }
 
     private OnBindItemView onBindItemView;
@@ -28,5 +46,25 @@ public class DefaultAdapter extends DefaultRVAdapter {
 
     public interface OnBindItemView {
         void onBindItemViewHolder(DefaultViewHolder holder, int position);
+    }
+
+    private OnBindItemClick onBindItemClick;
+
+    public void setOnBindItemClick(OnBindItemClick onBindItemClick) {
+        this.onBindItemClick = onBindItemClick;
+    }
+
+    public interface OnBindItemClick {
+        void onBindItemClick(View view, int position);
+    }
+
+    private OnBindItemLongClick onBindItemLongClick;
+
+    public void setOnBindItemLongClick(OnBindItemLongClick onBindItemLongClick) {
+        this.onBindItemLongClick = onBindItemLongClick;
+    }
+
+    public interface OnBindItemLongClick {
+        void onBindItemLongClick(View view, int position);
     }
 }
